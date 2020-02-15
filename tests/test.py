f77a04e1ed90629586ad916480602ff04958ca95
Shota Kato <34226464+stktKU@users.noreply.github.com>
2020-02-15 13:36:23 +0900

    add stanfordnlp_resources to data and change related codes.

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 import stanfordnlp
 
-nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos')
+# if system cannot load model, download model as below.
+# stanfordnlp.download('en', resource_dir='/project-mir/data/stanfordnlp_resources')   # This downloads the English models for the neural pipeline
+nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos', models_dir='./data/stanfordnlp_resources')
 doc = nlp("Barack Obama was born in Hawaii.")
 print(*[f'word: {word.text+" "}\tupos: {word.upos}\txpos: {word.xpos}' for sent in doc.sentences for word in sent.words], sep='\n')
 
