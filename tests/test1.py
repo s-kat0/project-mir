f8d0b5ea58ed3c0d880dca276556c049a6c79763
Shota Kato <34226464+stktKU@users.noreply.github.com>
2020-03-03 01:45:50 +0900

    realized extracting identifiers and preprocessing doc.body.

--- tests/test1.py
+++ tests/test1.py
@@ -1,14 +1,16 @@
 import os
 from pathlib import Path
 import stanfordnlp
-# import projectmir
 
 # if system cannot load model, download model as below.
 # currentPath = Path(os.getcwd())
 # MODELS_DIR = os.path.join(currentPath, 'data/stanfordnlp_resources')
 # stanfordnlp.download('en', resource_dir='/project-mir/data/stanfordnlp_resources')   # This downloads the English models for the neural pipeline
-nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos,lemma,depparse', 
-                           models_dir='./data/stanfordnlp_resources',
+
+currentPath = Path(os.getcwd())
+MODELS_DIR = os.path.join(currentPath, 'data/stanfordnlp_resources')
+nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos',#lemma,depparse', 
+                           models_dir=MODELS_DIR,
                            )
 doc = nlp("Barack Obama was born in Hawaii.")
 # print(*[f'word: {word.text+" "}\tupos: {word.upos}\txpos: {word.xpos}' for sent in doc.sentences for word in sent.words], sep='\n')
